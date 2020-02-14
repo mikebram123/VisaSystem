a1d9410fb677430eeb86d4d7e2a9fcd3c4e88427
michael61296 <michael61296@GB-LD1LPW10G003.mastek.com>
2020-02-14 15:15:49 +0000

    Created Project Structure

---
+++ visaSystem/src/main/java/com/optimizePrime/visaSystem/VisaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.optimizePrime.visaSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisaSystemApplication.class, args);
+	}
+
+}
---
+++ visaSystem/src/test/java/com/optimizePrime/visaSystem/VisaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimizePrime.visaSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
