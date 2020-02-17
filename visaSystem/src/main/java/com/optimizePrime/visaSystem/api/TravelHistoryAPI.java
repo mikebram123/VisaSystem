863feff073e366d403c7858e64a3591699e277b0
Hellen61291 <Hellen61291@GB-LD1LPW10G010.mastek.com>
2020-02-17 11:45:31 +0000

    Travel History Completed

--- visaSystem/src/main/java/com/optimizePrime/visaSystem/api/TravelHistoryAPI.java
+++ visaSystem/src/main/java/com/optimizePrime/visaSystem/api/TravelHistoryAPI.java
@@ -11,6 +11,23 @@ import javax.ws.rs.core.MediaType;
 
 import com.optimizePrime.visaSystem.entities.TravelHistory;
 
+@Path("/visaSystem")
 public interface TravelHistoryAPI {
+	
+	@GET
+	@Path("travel history/list")
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Iterable<TravelHistory>listAllTravelHistory();
+	
+	@GET
+	@Path("travel history/find/{travelHistoryId}")
+	@Produces({MediaType.APPLICATION_JSON})
+	public TravelHistory findByTravelHistoryId(@PathParam("travelHistoryId")int travelHistoryId);
+	
+	@POST
+	@Path("travelHistory/register")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	public TravelHistory registerNewTravelHistory(@BeanParam TravelHistory newTravelHistory);
 
 }
