414ca1b6c1db9be48d3e1fe353db1c2a5143164b
amy61289 <amy61289@GB-LD1LPW10G006.mastek.com>
2020-02-17 12:23:59 +0000

    Created Dependant entity, DAO, API, service and an add dependant test case.

--- visaSystem/src/test/java/com/optimizePrime/visaSystem/VisaSystemApplicationTests.java
+++ visaSystem/src/test/java/com/optimizePrime/visaSystem/VisaSystemApplicationTests.java
@@ -1,13 +1,40 @@
 package com.optimizePrime.visaSystem;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.optimizePrime.visaSystem.entities.Dependant;
+import com.optimizePrime.visaSystem.entities.dependantRelationship;
+import com.optimizePrime.visaSystem.services.DependentServices;
 
 @SpringBootTest
 class VisaSystemApplicationTests {
+	
+	@Autowired
+	DependentServices dependSvc1;
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void testDependentDAOAdd() {
+		Dependant depend = new Dependant();
+		depend.setDateOfBirth("14/08/1997");
+		depend.setGivenNames("Amy Louise");
+		depend.setFamilyName("Leake");
+		depend.setRelationshipToYou(dependantRelationship.SIBLING);
+		depend.setCountryOfNationality("UK");
+		depend.setAlwaysHadSameNationality(true);
+		
+		depend = dependSvc1.save(depend);
+		
+		System.out.println(depend);
+		assertNotNull(depend, "Dependent Added");
+		
 	}
 
 }
