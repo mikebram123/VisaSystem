6e81c3760949194a6c5850b507d4a7392abbd21d
Hellen61291 <Hellen61291@GB-LD1LPW10G010.mastek.com>
2020-02-19 08:49:04 +0000

    AService Improvements Completed

--- visaSystem/src/main/java/com/optimizePrime/visaSystem/services/AService.java
+++ visaSystem/src/main/java/com/optimizePrime/visaSystem/services/AService.java
@@ -16,41 +16,31 @@ import org.springframework.stereotype.Component;
 public class AService {
 
 //Method that we will use to get current date
-	public static boolean inLast10Years(String Date) {
+	public static boolean inLast10Years(String Date) {//parameter using to find data in Last 10years
 		double years = 0;
-		try { 				//example 
-			String string = "20-02-2020T10:20:00";//comes from the param of date
-
-			Date date = new SimpleDateFormat("dd-MM-yyyy'T'HH:mm:ss").parse(Date); //convert DB date from string to Date object
-			Date currentDate = new Date(); //gets current date
+		try {
+			//this converts DataBase date from String to Date Object
+			Date date = new SimpleDateFormat("dd-MM-yyyy'T'HH:mm:ss").parse(Date); 
+			//this gets the current date as in todays date.
+			Date currentDate = new Date(); 
 
 			long diffMil = currentDate.getTime() - date.getTime(); //get miliseconds between dates
-			long days = TimeUnit.MILLISECONDS.toDays(diffMil); //get days between dates
-			years = days / 365.25; //gets years between dates
-			// so meantion conflict in timezones between data in database and what we have in here
-			//may not be an issue *
-
+			long days = TimeUnit.MILLISECONDS.toDays(diffMil); //gets the days between dates
+			years = days / 365.25; //gets the years between dates
+			
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 
-		return years <= 10;
+		return years >= 10; //return date if its less than 10years or equals to ten years.
 	}
-	public static Instant getDateFromString(String string)
-	{
-		Instant timeStamp = null;
-
-		timeStamp=Instant.parse(string);
-
-		return timeStamp;
-	}
-
+	
 	public static void main(String[]args)
 	{
 
-		String exampledate = "13-05-2028T10:20:00";
+		String exampledate = "13-05-2010T10:20:00";
 		
 		System.out.println(inLast10Years(exampledate));//make call from method
-}
-
+		
+	}
 }
