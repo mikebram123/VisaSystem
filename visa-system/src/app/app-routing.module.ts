d45a2abba9e0ac85aa5a13209051c85a2e511524
Atiq Rafiq <Atiq61299@Mastek.Com>
2020-03-10 14:27:48 +0000

    done the home routing

--- visa-system/src/app/app-routing.module.ts
+++ visa-system/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { ApplicantComponent } from './applicant/applicant.component';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"applicant",component:ApplicantComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
